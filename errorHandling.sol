23ec40230c7dbaa95d67a7835fc3bf93d3c3fd12
Mr. Coder <54631960+OXYMORON16@users.noreply.github.com>
2023-03-16 01:03:09 +0530

    Create errorHandling.sol

---
+++ errorHandling.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.3;
+
+contract ExampleContract {
+
+    function sum(uint256 a, uint256 b) public pure returns (uint256) {
+        uint256 c  = a + b;
+        assert(c >= a && c >= b);
+        return c;
+    }
+
+    function divide(uint256 a, uint256 b) public pure returns (uint256) {
+        require(b != 0, "Cannot divide by zero");
+        return a / b;
+    }
+
+    function fail(uint256 a, uint256 b) public pure returns (uint256){
+        if(b == 0){
+            revert("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
