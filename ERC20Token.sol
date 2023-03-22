c21cee3d19b4247fa3c8394edec6f98f8cd5b6f1
Mr. Coder <54631960+OXYMORON16@users.noreply.github.com>
2023-03-22 18:39:23 +0530

    Create ERC20Token.sol

---
+++ ERC20Token.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract mytoken is ERC20 {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _mint(msg.sender, 100 * 10 ** 18); 
+    }
+}
